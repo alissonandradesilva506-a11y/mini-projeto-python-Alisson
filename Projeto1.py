dccb9c37dd177ddabe0da36fd0a0451ed0d2fa62
alissonandradesilva506-a11y <alissonandradesilva506@gmail.com>
2025-11-02 20:52:55 -0300

    Projeto1.py

---
+++ Projeto1.py
@@ -0,0 +1,12 @@
+produtos = []
+codigos_produtos = set()
+categorias =("Alimentos", "Bebidas", "Limpeza", "Utensílios Domésticos")
+
+while True:
+  print("//////MENU PRINCIPAL\\\\\\")
+  print("1- produto")
+  print("2- produto")
+  print("3- Buscar produto")
+  print("4- Atualizar produto")
+  print("5- Excluir produto")
+  print("0- Sair")
